6003c5171a05cff072550a439a983a654ad246b1
Jen_Tran <jentran.gamedeveloper@gmail.com>
2025-03-11 22:37:26 +0700

    Add files about students

---
+++ main_student.py
@@ -0,0 +1,6 @@
+from Example01.VietnameseStudent_HCMUTE import VietnameseStudent_HCMUTE
+
+student1 = VietnameseStudent_HCMUTE("Nguyễn Ngọc Gia Mẫn", "07/09/2004", "Female", "HCMUTE", 
+                             "34xx3983xxx" "TPHCM", "District 4", "22119197", "abcd@xxxxx.vn",
+                              "CN-KTMT", "7.0", "image.jpg" )
+student1.showBackground()
